a67be81b338f581115e944d6ea2a6856d7922c9b
AaronWard <aaronlaptop12@hotmail.com>
2018-05-23 15:08:02 +0100

    cleaned code

---
+++ README.md
@@ -0,0 +1,6 @@
+# amazon alexa skill
+
+This is an amazon alexa skill that uses the RTPI api for getting the real time bus information for a stop near me.
+The backend code is deployed to a lambda function on AWS and is enabled by the invocation phrase: "my bus checker" 
+
+![](https://cdn.geekwire.com/wp-content/uploads/2016/03/AmazonEchoDotFront-630x499.jpg)
--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,7 +1,5 @@
 var Alexa = require('alexa-sdk');
 var https = require('https');
-// var request = require('request');
-
 
 exports.handler = function(event, context, callback){
   var alexa = Alexa.handler(event, context);
@@ -12,24 +10,6 @@ exports.handler = function(event, context, callback){
 var handlers = {
   'LaunchRequest': function () {
     this.emit('GetBusses');
-  },
-  'GetAstros': function() {
-    getAstrosHttp((data) => {
-      var outputSpeech = `There are currently ${data.people.length} astronauts in space. `;
-      for (var i=0;i<data.people.length;i++){
-        if (i === 0) {
-          //first record
-          outputSpeech = outputSpeech + 'Their names are: ' + data.people[i].name + ', '
-        } else if (i === data.people.length-1) {
-          //last record
-          outputSpeech = outputSpeech + 'and ' + data.people[i].name + '.'
-        } else {
-          //middle record(s)
-          outputSpeech = outputSpeech + data.people[i].name + ', '
-        }
-      }
-      this.emit(':tell', outputSpeech);
-  });
   },
   'GetBusses' : function() {
     var responseString = '';
@@ -99,27 +79,3 @@ var handlers = {
 };
 
 /******************************************************************************* */
-function getAstrosHttp(callback) {
-  //http://api.open-notify.org/astros.json
-  var options = {
-    host: 'api.open-notify.org',
-    port: 80,
-    path: '/astros.json',
-    method: 'GET'
-  };
-
-  var req = http.request(options, res => {
-      res.setEncoding('utf8');
-      var returnData = "";
-
-      res.on('data', chunk => {
-          returnData = returnData + chunk;
-      });
-
-      res.on('end', () => {
-        var result = JSON.parse(returnData);
-        callback(result);
-      });
-  });
-  req.end();
-}
